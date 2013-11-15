61645a3e1ed35a82a0f5fd01be38abb1c9b88e2f
gsz <gsizeg@gmail.com>
2013-11-15 14:25:12 +0800

    add disqus

--- js/post.js
+++ js/post.js
@@ -1,215 +1,230 @@
 $(document).ready(function(){
 
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        }
-        ,BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        }
-        ,iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        }
-        ,Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        }
-        ,Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        }
-        ,any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		}
+				 ,BlackBerry: function() {
+					 return navigator.userAgent.match(/BlackBerry/i);
+				 }
+				 ,iOS: function() {
+					 return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+				 }
+				 ,Opera: function() {
+					 return navigator.userAgent.match(/Opera Mini/i);
+				 }
+				 ,Windows: function() {
+					 return navigator.userAgent.match(/IEMobile/i);
+				 }
+				 ,any: function() {
+					 return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+				 }
+	};
 
-    $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
+	$('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    //***********************
-    //**评论的代码也删掉哦***
-    window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
-    //**评论的代码也删掉哦***
-    //***********************
 
-    $('.entry a').each(function(index,element){
-        var href = $(this).attr('href');
-        if(href){
-            if(href.indexOf('#') == 0){
-            }else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('beiyuu.com')>-1 ){
-            }else if ($(element).has('img').length){
-            }else{
-                $(this).attr('target','_blank');
-                $(this).addClass('external');
-            }
-        }
-    });
+	<div id="disqus_thread"></div>
+		<script type="text/javascript">
+		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+		var disqus_shortname = 'gsize'; // required: replace example with your forum shortname 这个地方需要改成你配置的网站名
+	/* * * DON'T EDIT BELOW THIS LINE * * */
+	(function() {
+		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+		dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	})();
+	</script>
+		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+		<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
 
-    (function(){
-        var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
+		//***********************
+		//**评论的代码也删掉哦***
+		//    window.disqus_shortname = 'gsize'; // required: replace example with your forum shortname
+		//    $('#disqus_container .comment').on('click',function(){
+		//        $(this).html('加载中...');
+		//        var that = this;
+		//        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+		//    });
+		//**评论的代码也删掉哦***
+		//***********************
 
-        function initHeading(){
-            var h2 = [];
-            var h3 = [];
-            var h2index = 0;
+		$('.entry a').each(function(index,element){
+			var href = $(this).attr('href');
+			if(href){
+				if(href.indexOf('#') == 0){
+				}else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('beiyuu.com')>-1 ){
+				}else if ($(element).has('img').length){
+				}else{
+					$(this).attr('target','_blank');
+					$(this).addClass('external');
+				}
+			}
+		});
 
-            $.each($('.entry h2, .entry h3'),function(index,item){
-                if(item.tagName.toLowerCase() == 'h2'){
-                    var h2item = {};
-                    h2item.name = $(item).text();
-                    h2item.id = 'menuIndex'+index;
-                    h2.push(h2item);
-                    h2index++;
-                }else{
-                    var h3item = {};
-                    h3item.name = $(item).text();
-                    h3item.id = 'menuIndex'+index;
-                    if(!h3[h2index-1]){
-                        h3[h2index-1] = [];
-                    }
-                    h3[h2index-1].push(h3item);
-                }
-                item.id = 'menuIndex' + index;
-            });
+	(function(){
+		var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
 
-            return {h2:h2,h3:h3}
-        }
+		function initHeading(){
+			var h2 = [];
+			var h3 = [];
+			var h2index = 0;
 
-        function genTmpl(){
-            var h1txt = $('h1').text();
-            var tmpl = '<ul><li class="h1"><a href="#">' + h1txt + '</a></li>';
+			$.each($('.entry h2, .entry h3'),function(index,item){
+				if(item.tagName.toLowerCase() == 'h2'){
+					var h2item = {};
+					h2item.name = $(item).text();
+					h2item.id = 'menuIndex'+index;
+					h2.push(h2item);
+					h2index++;
+				}else{
+					var h3item = {};
+					h3item.name = $(item).text();
+					h3item.id = 'menuIndex'+index;
+					if(!h3[h2index-1]){
+						h3[h2index-1] = [];
+					}
+					h3[h2index-1].push(h3item);
+				}
+				item.id = 'menuIndex' + index;
+			});
 
-            var heading = initHeading();
-            var h2 = heading.h2;
-            var h3 = heading.h3;
+			return {h2:h2,h3:h3}
+		}
 
-            for(var i=0;i<h2.length;i++){
-                tmpl += '<li><a href="#" data-id="'+h2[i].id+'">'+h2[i].name+'</a></li>';
+		function genTmpl(){
+			var h1txt = $('h1').text();
+			var tmpl = '<ul><li class="h1"><a href="#">' + h1txt + '</a></li>';
 
-                if(h3[i]){
-                    for(var j=0;j<h3[i].length;j++){
-                        tmpl += '<li class="h3"><a href="#" data-id="'+h3[i][j].id+'">'+h3[i][j].name+'</a></li>';
-                    }
-                }
-            }
-            tmpl += '</ul>';
+			var heading = initHeading();
+			var h2 = heading.h2;
+			var h3 = heading.h3;
 
-            return tmpl;
-        }
+			for(var i=0;i<h2.length;i++){
+				tmpl += '<li><a href="#" data-id="'+h2[i].id+'">'+h2[i].name+'</a></li>';
 
-        function genIndex(){
-            var tmpl = genTmpl();
-            var indexCon = '<div id="menuIndex" class="sidenav"></div>';
+				if(h3[i]){
+					for(var j=0;j<h3[i].length;j++){
+						tmpl += '<li class="h3"><a href="#" data-id="'+h3[i][j].id+'">'+h3[i][j].name+'</a></li>';
+					}
+				}
+			}
+			tmpl += '</ul>';
 
-            $('#content').append(indexCon);
+			return tmpl;
+		}
 
-            $('#menuIndex')
-                .append($(tmpl))
-                .delegate('a','click',function(e){
-                    e.preventDefault();
+		function genIndex(){
+			var tmpl = genTmpl();
+			var indexCon = '<div id="menuIndex" class="sidenav"></div>';
 
-                    var selector = $(this).attr('data-id') ? '#'+$(this).attr('data-id') : 'h1'
-                    var scrollNum = $(selector).offset().top;
+			$('#content').append(indexCon);
 
-                    $('body, html').animate({ scrollTop: scrollNum-30 }, 400, 'swing');
-                });
-        }
+			$('#menuIndex')
+				.append($(tmpl))
+				.delegate('a','click',function(e){
+					e.preventDefault();
 
-        var waitForFinalEvent = (function () {
-            var timers = {};
-            return function (callback, ms, uniqueId) {
-                if (!uniqueId) {
-                    uniqueId = "Don't call this twice without a uniqueId";
-                }
-                if (timers[uniqueId]) {
-                    clearTimeout (timers[uniqueId]);
-                }
-                timers[uniqueId] = setTimeout(callback, ms);
-            };
-        })();
+					var selector = $(this).attr('data-id') ? '#'+$(this).attr('data-id') : 'h1'
+					var scrollNum = $(selector).offset().top;
 
-        if($('.entry h2').length > 2 && !isMobile.any() && !ie6){
+				$('body, html').animate({ scrollTop: scrollNum-30 }, 400, 'swing');
+				});
+		}
 
-            genIndex();
+		var waitForFinalEvent = (function () {
+			var timers = {};
+			return function (callback, ms, uniqueId) {
+				if (!uniqueId) {
+					uniqueId = "Don't call this twice without a uniqueId";
+				}
+				if (timers[uniqueId]) {
+					clearTimeout (timers[uniqueId]);
+				}
+				timers[uniqueId] = setTimeout(callback, ms);
+			};
+		})();
 
-            $(window).load(function(){
-                var scrollTop = [];
-                $.each($('#menuIndex li a'),function(index,item){
-                    var selector = $(item).attr('data-id') ? '#'+$(item).attr('data-id') : 'h1'
-                    var top = $(selector).offset().top;
-                    scrollTop.push(top);
-                });
+		if($('.entry h2').length > 2 && !isMobile.any() && !ie6){
 
-                var menuIndexTop = $('#menuIndex').offset().top;
-                var menuIndexLeft = $('#menuIndex').offset().left;
+			genIndex();
 
-                $(window).scroll(function(){
-                    waitForFinalEvent(function(){
-                        var nowTop = $(window).scrollTop();
-                        var length = scrollTop.length;
-                        var index;
+			$(window).load(function(){
+				var scrollTop = [];
+				$.each($('#menuIndex li a'),function(index,item){
+					var selector = $(item).attr('data-id') ? '#'+$(item).attr('data-id') : 'h1'
+					var top = $(selector).offset().top;
+				scrollTop.push(top);
+				});
 
-                        if(nowTop+20 > menuIndexTop){
-                            $('#menuIndex').css({
-                                position:'fixed'
-                                ,top:'20px'
-                                ,left:menuIndexLeft
-                            });
-                        }else{
-                            $('#menuIndex').css({
-                                position:'static'
-                                ,top:0
-                                ,left:0
-                            });
-                        }
+				var menuIndexTop = $('#menuIndex').offset().top;
+				var menuIndexLeft = $('#menuIndex').offset().left;
 
-                        if(nowTop+60 > scrollTop[length-1]){
-                            index = length;
-                        }else{
-                            for(var i=0;i<length;i++){
-                                if(nowTop+60 <= scrollTop[i]){
-                                    index = i;
-                                    break;
-                                }
-                            }
-                        }
-                        $('#menuIndex li').removeClass('on');
-                        $('#menuIndex li').eq(index-1).addClass('on');
-                    });
-                });
+				$(window).scroll(function(){
+					waitForFinalEvent(function(){
+						var nowTop = $(window).scrollTop();
+						var length = scrollTop.length;
+						var index;
 
-                $(window).resize(function(){
-                    $('#menuIndex').css({
-                        position:'static'
-                        ,top:0
-                        ,left:0
-                    });
+						if(nowTop+20 > menuIndexTop){
+							$('#menuIndex').css({
+								position:'fixed'
+								,top:'20px'
+								,left:menuIndexLeft
+							});
+						}else{
+							$('#menuIndex').css({
+								position:'static'
+								,top:0
+								,left:0
+							});
+						}
 
-                    menuIndexTop = $('#menuIndex').offset().top;
-                    menuIndexLeft = $('#menuIndex').offset().left;
+						if(nowTop+60 > scrollTop[length-1]){
+							index = length;
+						}else{
+							for(var i=0;i<length;i++){
+								if(nowTop+60 <= scrollTop[i]){
+									index = i;
+									break;
+								}
+							}
+						}
+						$('#menuIndex li').removeClass('on');
+						$('#menuIndex li').eq(index-1).addClass('on');
+					});
+				});
 
-                    $(window).trigger('scroll')
-                    $('#menuIndex').css('max-height',$(window).height()-80);
-                });
-            })
+				$(window).resize(function(){
+					$('#menuIndex').css({
+						position:'static'
+						,top:0
+						,left:0
+					});
 
-            //用js计算屏幕的高度
-            $('#menuIndex').css('max-height',$(window).height()-80);
-        }
-    })();
+					menuIndexTop = $('#menuIndex').offset().top;
+					menuIndexLeft = $('#menuIndex').offset().left;
 
-    $.getScript('/js/prettify/prettify.js',function(){
-        prettyPrint();
-    });
+					$(window).trigger('scroll')
+					$('#menuIndex').css('max-height',$(window).height()-80);
+				});
+			})
 
-    if(/\#comment/.test(location.hash)){
-        $('#disqus_container .comment').trigger('click');
-    }
+			//用js计算屏幕的高度
+			$('#menuIndex').css('max-height',$(window).height()-80);
+		}
+	})();
 
-    if(/css3-animation/.test(location.href)){
-        $("head").append("<link rel='stylesheet' type='text/css' href='/css/css3-ani.css'/>");
-        $.getScript('/js/css3-ani.js',function(){});
-    }
+	$.getScript('/js/prettify/prettify.js',function(){
+		prettyPrint();
+	});
+
+	if(/\#comment/.test(location.hash)){
+		$('#disqus_container .comment').trigger('click');
+	}
+
+	if(/css3-animation/.test(location.href)){
+		$("head").append("<link rel='stylesheet' type='text/css' href='/css/css3-ani.css'/>");
+		$.getScript('/js/css3-ani.js',function(){});
+	}
 });
