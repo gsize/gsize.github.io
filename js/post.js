340607958fbf5d1a9df2ad78b3b63187bd46ac8e
gsz <gsizeg@gmail.com>
2013-11-15 14:31:43 +0800

    fix disqus

--- js/post.js
+++ js/post.js
@@ -24,28 +24,28 @@ $(document).ready(function(){
 	$('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
 
-	<div id="disqus_thread"></div>
-		<script type="text/javascript">
+//	<div id="disqus_thread"></div>
+//		<script type="text/javascript">
 		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-		var disqus_shortname = 'gsize'; // required: replace example with your forum shortname 这个地方需要改成你配置的网站名
+//		var disqus_shortname = 'gsize'; // required: replace example with your forum shortname 这个地方需要改成你配置的网站名
 	/* * * DON'T EDIT BELOW THIS LINE * * */
-	(function() {
-		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	})();
-	</script>
-		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-		<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+//	(function() {
+//		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+//		dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+//		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+//	})();
+//	</script>
+//		<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+//		<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
 
 		//***********************
 		//**评论的代码也删掉哦***
-		//    window.disqus_shortname = 'gsize'; // required: replace example with your forum shortname
-		//    $('#disqus_container .comment').on('click',function(){
-		//        $(this).html('加载中...');
-		//        var that = this;
-		//        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-		//    });
+		    window.disqus_shortname = 'gsize'; // required: replace example with your forum shortname
+		    $('#disqus_container .comment').on('click',function(){
+		        $(this).html('加载中...');
+		        var that = this;
+		        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+		    });
 		//**评论的代码也删掉哦***
 		//***********************
 
